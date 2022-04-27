ce1165bf00aaa838154474829bcf1980b9eed4d1
victoricoma <victorsena3010@gmail.com>
2022-04-27 16:51:10 -0300

    Soulução do execício de aula!

--- aula1/ImprimeIdade.java
+++ aula1/ImprimeIdade.java
@@ -1,32 +1,31 @@
 package aula1;
 
-public class ImprimeIdade
-{
+public class ImprimeIdade {
     public static void main(String[] args) {
-        int idade=40;
-        String nome ="Victor Icoma"; 
+        int idade = 40;
+        String nome = "Victor Icoma";
         boolean acompanhado = false;
         double altura = 1.82;
         double peso = 125.7;
-        double imc = (peso)/(altura*altura);
+        double imc = (peso) / (altura * altura);
 
-        if(idade >= 40){
-            if(imc<22){
-                System.out.println("IMC= "+imc+" fique atento com sua sub-alimentação.");
-            }else if(imc <= 30){
-                System.out.println("IMC= "+imc+" Meus parabéns está em forma.");
-            }else if(imc <= 35){
-                System.out.println("IMC= "+imc+" Cuidado está com sobre peso.");
-            }else{
-                System.out.println("IMC= "+imc+" Esta com obesidade procure um médico.");
+        if (idade >= 40) {
+            if (imc < 22) {
+                System.out.println("IMC= " + imc + " fique atento com sua sub-alimentação.");
+            } else if (imc <= 30) {
+                System.out.println("IMC= " + imc + " Meus parabéns está em forma.");
+            } else if (imc <= 35) {
+                System.out.println("IMC= " + imc + " Cuidado está com sobre peso.");
+            } else {
+                System.out.println("IMC= " + imc + " Esta com obesidade procure um médico.");
             }
-        }else{
-            if(imc<25){
-                System.out.println("IMC= "+imc+" Se alimente melhor e cuidado com desnutrição.");
-            }else if(imc <= 35){
-                System.out.println("IMC= "+imc+" Meus parabéns está em forma.");
-            }else{
-                System.out.println("IMC= "+imc+" Planeje alguns exercícios.");
+        } else {
+            if (imc < 25) {
+                System.out.println("IMC= " + imc + " Se alimente melhor e cuidado com desnutrição.");
+            } else if (imc <= 35) {
+                System.out.println("IMC= " + imc + " Meus parabéns está em forma.");
+            } else {
+                System.out.println("IMC= " + imc + " Planeje alguns exercícios.");
             }
         }
 
