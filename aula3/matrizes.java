7612d45d5f4aa45db8ddd82e51ff0c7bb5964fdd
victoricoma <victorsena3010@gmail.com>
2022-04-29 15:28:34 -0300

    Execicios de Matrizes

--- aula3/matrizes.java
+++ aula3/matrizes.java
@@ -2,30 +2,35 @@ package aula3;
 
 import java.util.ArrayList;
 
+
 public class matrizes {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> matriz = new ArrayList<>();
 
-        int c=3;
+        int c=2;
 
         for(int i=0; i< c ; i++){
             matriz.add(new ArrayList<>());
         }
 
-        matriz.get(0).add(12);
-        matriz.get(0).add(22);
-        matriz.get(0).add(14);
-        matriz.get(1).add(52);
-        matriz.get(1).add(37);
-        matriz.get(1).add(78);
-        matriz.get(2).add(87);
-        matriz.get(2).add(62);
-        matriz.get(2).add(42);
-        
+        //#region Abastecimento da Matriz
+        matriz.get(0).add(1);
+        matriz.get(0).add(1);
+        matriz.get(0).add(15);
+        matriz.get(1).add(4);
+        matriz.get(1).add(-2);
+        matriz.get(1).add(12);
+        //#endregion
+
+        //#region Organização das Matrizes
         System.out.println(matriz.get(0));
         System.out.println(matriz.get(1));
-        System.out.println(matriz.get(2));
         
-        System.out.println("[1, 1]= "+matriz.get(1).get(1));
+        double y=((matriz.get(1).get(0)*matriz.get(0).get(2))-matriz.get(1).get(2))/(matriz.get(1).get(0)+matriz.get(0).get(0));
+        double x=(matriz.get(0).get(2)-y);
+        //#endregion
+
+        System.out.println("Y= "+y);
+        System.out.println("X= "+x);
     }
 }
