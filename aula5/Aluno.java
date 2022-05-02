db10ea7a7326a8dd405ba66301b7782222dc5085
victoricoma <victorsena3010@gmail.com>
2022-05-02 11:53:52 -0300

    Calsses com herança de atributos e estrutura.

---
+++ aula5/Aluno.java
@@ -0,0 +1,31 @@
+package aula5;
+
+public class Aluno extends Pessoa{
+    private String curso;
+    private double[] notas;
+
+    //#region Encapsulamento [Getters e Setters]
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+    public double[] getNotas() {
+        return notas;
+    }
+    //#endregion
+
+    //#region Regras de Negócios
+    public double calculaMedia(){
+        return 0;
+    }
+
+    public boolean verificaAprovacao(){
+        return true;
+    }
+    //#endregion
+}
---
+++ aula5/Professor.java
@@ -0,0 +1,26 @@
+package aula5;
+
+public class Professor extends Pessoa{
+    private double salario;
+    private String nomeCurso;
+
+    //#region Encapsulamento [Getters e Setters]
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+    //#endregion
+    //#region Regras de Negócios
+    public double calculaSalario(){
+        return 0;
+    }
+    //#endregion
+}
